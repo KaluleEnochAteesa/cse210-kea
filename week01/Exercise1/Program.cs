ecbfc5962162da12a5f63ff08b401b3bfb333474
KaluleEnochAteesa <kal24017@byui.edu>
2025-01-13 11:21:03 +0300

    exercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("Please enter first_name;");
+        string first_name=Console.ReadLine();
+        Console.WriteLine("Please enter last_name;");
+        string last_name=Console.ReadLine();
+        Console.WriteLine($"Your name is {last_name},{first_name} {last_name}.");
     }
 }
+    
