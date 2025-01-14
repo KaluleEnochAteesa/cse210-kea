0ec07f20a0cc4c4cabcd729acd71af663692d1f3
KaluleEnochAteesa <kal24017@byui.edu>
2025-01-14 12:30:03 +0300

    exerc4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+
+        int sum = 0;
+        int largest = int.MinValue;
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+
+        if (numbers.Count > 0)
+        {
+            double average = (double)sum / numbers.Count;
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
+
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
