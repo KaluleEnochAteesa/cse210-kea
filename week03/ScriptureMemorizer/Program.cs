7b2ebe11a20c1d35d021003a8ba64651d507ebd6
KaluleEnochAteesa <kal24017@byui.edu>
2025-01-23 23:22:31 +0300

    prac submit

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,44 @@
+/* The program  loads scriptures from a file so it exceeds requirements*/
+
+
+
 using System;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        string scriptureText = LoadScriptureFromFile("scripture.txt");
+        Reference reference = new Reference("John", 3, 16);
+        Scripture scripture = new Scripture(reference, scriptureText);
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("Press Enter to hide words or type 'quit' to exit.");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.Clear();
+                Console.WriteLine(scripture.GetDisplayText());
+                break;
+            }
+        }
+    }
+
+    static string LoadScriptureFromFile(string filePath)
+    {
+        return File.ReadAllText(filePath);
     }
 }
