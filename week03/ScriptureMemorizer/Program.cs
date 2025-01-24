a023475b94d1912d8fc3868eda65e06235f98217
KaluleEnochAteesa <kal24017@byui.edu>
2025-01-24 11:57:08 +0300

    scripture memo

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -10,6 +10,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        
         string scriptureText = LoadScriptureFromFile("scripture.txt");
         Reference reference = new Reference("John", 3, 16);
         Scripture scripture = new Scripture(reference, scriptureText);
